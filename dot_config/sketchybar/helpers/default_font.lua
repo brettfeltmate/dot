3eb85aa847295d59cbfaa4e4454558588eab3709
brettfeltmate <thebretteremail@gmail.com>
2024-07-06 20:20:33 -0300

    Aerospace, borders, & sketchybar

---
+++ dot_config/sketchybar/helpers/default_font.lua
@@ -0,0 +1,13 @@
+return {
+	text = "Maple Mono NF", -- Used for text
+	numbers = "Maple Mono NF", -- Used for numbers
+
+	-- Unified font style map
+	style_map = {
+		["Regular"] = "Medium",
+		["Semibold"] = "Semibold",
+		["Bold"] = "Bold",
+		["Heavy"] = "Bold",
+		["Black"] = "Bold",
+	},
+}
