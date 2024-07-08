64f36c15696d5c08b7ecfa9ece82d6e14f2a9064
brettfeltmate <thebretteremail@gmail.com>
2024-07-08 11:52:58 -0300

    R-related tweaks to formatter & linter

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,22 +1,21 @@
 return {
 	{ -- Autoformat
 		"stevearc/conform.nvim",
-		lazy = false,
+		event = "BufEnter",
 		opts = {
-			notify_on_error = false,
+			notify_on_error = true,
 			format_on_save = function(bufnr)
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = { c = true, cpp = true, r = true }
 				return {
-					timeout_ms = 2000,
+					timeout_ms = 5000,
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
 				}
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
-				markdown = { "prettier" },
 				python = { "blue" },
 				r = { "styler" },
 			},
