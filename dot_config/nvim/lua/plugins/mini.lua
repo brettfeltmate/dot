7fef33ad613bf2420adaadb72e76d077e96690c9
Brett Feltmate <brettfeltmate@MacBook-Air-4.local>
2024-10-23 18:21:09 -0300

    (add) atuin

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -4,44 +4,20 @@ return {
 	config = function()
 		require("mini.align").setup()
 		require("mini.ai").setup()
-		-- require("mini.base16").setup()
 		require("mini.basics").setup()
 		require("mini.bracketed").setup()
-		-- require("mini.bufremove").setup()
+		require("mini.bufremove").setup()
 		require("mini.comment").setup()
 		require("mini.extra").setup()
 		require("mini.indentscope").setup()
 		require("mini.misc").setup()
-		require("mini.move").setup({
-			mappings = {
-				left = "<S-left>",
-				right = "<S-right>",
-				down = "<S-down>",
-				up = "<S-up>",
-
-				line_left = "<S-left>",
-				line_right = "<S-right>",
-				line_down = "<S-down>",
-				line_up = "<S-up>",
-			},
-		})
-		-- require("mini.pairs").setup()
+		require("mini.move").setup()
+		require("mini.pairs").setup()
 		require("mini.trailspace").setup()
-
-		-- local animate = require("mini.animate")
-
-		require("mini.animate").setup({
-			cursor = { enable = false }, -- Wonky with ext-marked files (e.g., mark/help-view); too lazy to finagle
-
-			scroll = {}, -- use default
-			resize = { enable = false }, -- FIXME: find timing that doesn't suck ass for longer transitions
-			open = {}, -- don't actually know what this impacts
-		})
 
 		local clue = require("mini.clue")
 
 		require("mini.clue").setup({
-
 			window = {
 				delay = 300,
 				config = { width = "auto", border = "single" },
@@ -92,17 +68,14 @@ return {
 
 			clues = {
 				-- Enhance this by adding descriptions for <Leader> mapping groups
-				{ mode = "n", keys = "<leader>a", desc = "  Avante +|>" },
-				{ mode = "v", keys = "<leader>a", desc = "  Avante +|>" },
-				{ mode = "n", keys = "<leader>b", desc = "  Buffer +|>" },
-				{ mode = "n", keys = "<leader>s", desc = "  Search +|>" },
-				{ mode = "n", keys = "<leader>l", desc = "     Lsp +|>" },
-				{ mode = "n", keys = "<leader>g", desc = "     Git +|>" },
-				{ mode = "n", keys = "<leader>,", desc = "      UI +|>" },
-				{ mode = "n", keys = "<leader>w", desc = "  Window +|>" },
-				{ mode = "n", keys = "<leader>r", desc = "    REPL +|>" },
-				{ mode = "v", keys = "<leader>r", desc = "    REPL +|>" },
-				{ mode = "x", keys = "<leader>r", desc = "    REPL +|>" },
+				{ mode = "n", keys = "<leader>b", desc = "Buffer" },
+				{ mode = "n", keys = "<leader>c", desc = "Chat" },
+				{ mode = "v", keys = "<leader>c", desc = "Chat" },
+				{ mode = "n", keys = "<leader>s", desc = "Search" },
+				{ mode = "n", keys = "<leader>l", desc = "Lsp" },
+				{ mode = "n", keys = "<leader>g", desc = "Git" },
+				{ mode = "n", keys = "<leader>,", desc = "UI" },
+				{ mode = "n", keys = "<leader>w", desc = "Window" },
 
 				clue.gen_clues.builtin_completion(),
 				clue.gen_clues.g(),
