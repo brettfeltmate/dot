0b99643f5a608c073935779f8bf624957b72ddbe
Brett Feltmate <brettfeltmate@MacBookAir.ht.home>
2024-12-24 15:27:11 -0400

    (zsh) .rc & aliases cleanup
    .rc: reorganzied into sections (sort of)
    aliases: simplified eza shorthands

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -1,42 +1,26 @@
-alias ls="eza --color --icons --classify --group-directories-first"
-alias la="ls -a"
-alias ld="ls -lD"
-alias lf="ls -lf | grep -v /"
-alias ll="ls -al"
-alias lh="ls -dl .*"
-alias lm="ls -al --sort=modified"
-alias tre='tre -e nvim'
-alias tr1="tre -l 1"
-alias tr2="tre -l 2"
-alias tr3="tre -l 3"
-alias td1="tre -d -l 1"
-alias td2="tre -d -l 2"
-alias td3="tre -d -l 3"
-alias ta1="tre -a -l 1"
-alias ta2="tre -a -l 2"
-alias ta3="tre -a -l 3"
-# alias cd='z'
+# eza shorthands
+alias ls="eza --group-directories-first"
+alias la="ls --all --no-user --git"
+alias ll="la --long --grid"
+
+# quickly traverse upwards
 alias ..="cd '..' "
 alias ...="cd '../..'"
 alias ....="cd '../../..'"
+
 alias r="radian"
 alias lg="lazygit"
 alias yy="yazi"
-alias rr="ranger"
 alias olli="ansiweather -a true -p true -s true -h true -H true -s true -u metric -l 'Halifax,CA'"
 alias cat="bat"
 
-
-# dotfile management
+# chezmoi shorthands
 alias editmoi="chezmoi edit"
 alias addmoi="chezmoi add"
 alias raddmoi="chezmoi re-add"
 alias applymoi="chezmoi apply"
 alias cdmoi="chezmoi cd"
 alias delmoi="chezmoi destroy"
-
-alias msvim='NVIM_APPNAME="msvim" nvim'
-alias neoboox='NVIM_APPNAME="neoboox" nvim'
 
 # qol aliases when switching between archs
 alias arch86="/usr/bin/arch -x86_64 /usr/bin/env /bin/zsh --login; arch"
@@ -51,22 +35,7 @@ alias mux="tmux new-session -A -s default"
 alias tms="create_tmux_session"
 alias fab="fabular"
 
-# tmux
-# alias con="sesh connect $(sesh list | fzf)"
-# alias ses="sesh-sessions"
-#
-
-# taskwarrior
-alias tcal="task calendar"
-alias tnext="task next"
-alias ttui="taskwarrior-tui"
-alias tls="task ls"
-alias tdue="task due"
-alias tsum="task summary"
-
 alias icat="imgcat -W 100% -r"
-
-alias diet="NVIM_APPNAME='dietnvim' nvim"
 
 # visidata shorthands
 alias vd="visidata --theme=ascii8"
