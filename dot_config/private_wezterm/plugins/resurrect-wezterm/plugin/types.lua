92e4b134a856beacf64b9a653cc762ccc5ef40fc
Brett Feltmate <brettfeltmate@MacBook-Air-9.local>
2025-02-19 00:02:53 -0400

    Officially on the kitty train.

--- dot_config/private_wezterm/plugins/resurrect-wezterm/plugin/types.lua
+++
@@ -1,8 +0,0 @@
----@alias tab_size {rows: integer, cols: integer, pixel_width: integer, pixel_height: integer, dpi: integer}
----@alias workspace_state {workspace: string, window_states: window_state[]}
----@alias window_state {title: string, tabs: tab_state[], workspace: string, size: tab_size}
----@alias tab_state {title: string, pane_tree: pane_tree, is_active: boolean}
----@alias MuxTab any
----@alias MuxWindow any
-
----@alias restore_opts {relative: boolean?, absolute: boolean?, pane: Pane?, tab: MuxTab?, window: MuxWindow, on_pane_restore: fun(pane_tree: pane_tree)}
